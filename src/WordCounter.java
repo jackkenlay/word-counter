13edef5819b9543816ba37f927b02548d84f13ac
Jack Kenlay <jackkenlay@gmail.com>
2017-11-27 15:25:46 +0000

    init, with test unit tests

---
+++ src/WordCounter.java
@@ -0,0 +1,14 @@
+
+public class WordCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean testMe() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
---
+++ src/WordCounterTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordCounterTest {
+
+	@Test
+	public void firstTestExample() {
+		//fail("Not yet implemented");
+		WordCounter wordCounter = new WordCounter();
+		
+		assertTrue("testMe returns true",wordCounter.testMe());
+		
+	}
+
+}
