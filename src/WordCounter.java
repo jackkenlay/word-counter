bd7cfbc12012de384b30c2140f0b0319db0e9f97
Jack Kenlay <jackkenlay@gmail.com>
2017-11-28 11:11:51 +0000

    Working Example with tests

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class WordCounter {
 
@@ -5,9 +9,41 @@ public class WordCounter {
 		// TODO Auto-generated method stub
 	}
 	
-	public String[][] countWords(String inputSentance){
-		String[][] toReturn = null;
-		
+	public HashMap<String,Integer> countWords(String inputSentance){
+		String[] allWords = inputSentance.split(" ");
+		HashMap <String,Integer> toReturn = new HashMap <String,Integer>();
+
+		for(int i = 0; i < allWords.length; i++) {
+			String currentWord = allWords[i];
+			if(!isContainedInOtherWords(allWords, currentWord)) {
+				int count = getWordCount(allWords, currentWord);
+				toReturn.put(currentWord, count);
+			}
+		}
+
 		return toReturn;
+	}
+	
+	public boolean isContainedInOtherWords(String[] words, String inputWord){
+		boolean includedInOtherWord = false;
+
+		for(int i = 0; i < words.length; i++) {
+			if(words[i].contains(inputWord)&& words[i].length()!=inputWord.length()) {
+				includedInOtherWord = true;
+				break;
+			}
+		}
+
+		return includedInOtherWord;
+	}
+	
+	public int getWordCount(String[] words, String inputWord) {
+		int counter = 0;
+		for(int i = 0; i < words.length; i++) {
+			if(words[i].equals(inputWord)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 }
--- src/WordCounterTest.java
+++ src/WordCounterTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class WordCounterTest {
@@ -7,22 +11,52 @@ public class WordCounterTest {
 	@Test
 	public void firstTestExample() {
 		WordCounter wordCounter = new WordCounter();
-		String testString = "A mate material may maybe right maybe";
+		//change to upper case later on
+		String testString = "a mate material may maybe right maybe";
 		
-		String[][] expectedResponse = {{
-			"material","1"
-		},{
-			"maybe","2"
-		},{
-			"right","1"
-		}};
+		HashMap<String, Integer> expectedResponse = new HashMap<String,Integer>();
 		
-		String[][] response = wordCounter.countWords(testString);
+		expectedResponse.put("material", 1);
+		expectedResponse.put("maybe", 2);
+		expectedResponse.put("right", 1);
 		
-		for(int i = 0; i < response.length; i++) {
-			for(int p = 0; p < response[i].length; p++) {
-				assertEquals(response[i][p],expectedResponse[i][p]);
-			}
-		}
+		HashMap<String, Integer> response = wordCounter.countWords(testString);
+
+		assertEquals(response.get("material"),(Integer)1);
+		assertEquals(response.get("maybe"),(Integer)2);
+		assertEquals(response.get("right"),(Integer)1);
+		assertEquals(response.get("a"),null);
+	}
+	
+	@Test
+	public void isContainedInOtherWordsTest(){
+		String[] testStringArr = "a bag".split(" ");
+		String[] testStringArr2 = "a not".split(" ");
+		String[] testStringArr3 = "bag baggage baggled".split(" ");
+		
+		WordCounter wordCounter = new WordCounter();
+		
+		assertTrue(wordCounter.isContainedInOtherWords(testStringArr, "a"));
+		assertFalse(wordCounter.isContainedInOtherWords(testStringArr2, "a"));
+		assertTrue(wordCounter.isContainedInOtherWords(testStringArr3, "bag"));
+		assertFalse(wordCounter.isContainedInOtherWords(testStringArr2, "not"));
+	}
+	
+	@Test
+	public void getWordCountTest() {
+		String[] testStringArr = "a bag".split(" ");
+		String[] testStringArr2 = "a not".split(" ");
+		String[] testStringArr3 = "bag baggage baggled bag".split(" ");
+		
+		WordCounter wordCounter = new WordCounter();
+		
+		assertEquals(wordCounter.getWordCount(testStringArr, "a"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr, "bag"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr2, "a"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr2, "not"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr3, "bag"),2);
+		assertEquals(wordCounter.getWordCount(testStringArr3, "baggage"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr3, "baggled"),1);
+		assertEquals(wordCounter.getWordCount(testStringArr3, "a"),0);
 	}
 }
