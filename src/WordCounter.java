441c000a221f1444e2b8268278c4dd437189803d
Jack Kenlay <jackkenlay@gmail.com>
2017-11-28 15:48:12 +0000

    Changing it back

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -80,7 +80,7 @@ public class WordCounter {
 		Collections.sort(inputList, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length()<o2.length()){
+                if(o1.length()>o2.length()){
                     return 1;
                 }else{
                     return o1.compareTo(o2);
